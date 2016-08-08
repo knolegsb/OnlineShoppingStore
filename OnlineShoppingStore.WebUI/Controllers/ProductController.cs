2503ba20ca4e3b6395109737864491a25e985564
Goodsean <seon2you@hotmail.com>
2016-08-08 14:54:25 -0700

    category selection and modified pagination

--- OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -30,7 +30,7 @@ namespace OnlineShoppingStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(p => p.Category == category).Count()
                 },
                 CurrentCategory = category
             };
