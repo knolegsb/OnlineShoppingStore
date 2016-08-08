f055e098ae8b7bae2f779c1bc48ded7961316c62
Goodsean <seon2you@hotmail.com>
2016-08-08 13:46:59 -0700

    more route options

--- OnlineShoppingStore.WebUI/App_Start/RouteConfig.cs
+++ OnlineShoppingStore.WebUI/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace OnlineShoppingStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "{controller}/{action}/Page{page}",
-                defaults: new { controller = "Product", action = "List" }
-            );
+            routes.MapRoute(null, "", new { controller = "Product", action = "List", category = (string)null, page = 1 });
+
+            routes.MapRoute(null, "Page{page}", new { controller = "Product", action = "List", category = (string)null }, new { page = @"\d+" });
+
+            //routes.MapRoute(
+            //    name: null,
+            //    url: "{controller}/{action}/Page{page}",
+            //    defaults: new { controller = "Product", action = "List" }
+            //);
+
+            routes.MapRoute(null, "{category}", new { controller = "Product", action = "List", page = 1});
+
+            routes.MapRoute(null, "{category}/Page{page}", new { controller = "Product", action = "List" }, new { page = @"\d+" });
 
             routes.MapRoute(
                 name: "Default",
